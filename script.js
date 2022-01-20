cc9702608ce60aed689253d9ae1b717dfaa830b8
EmilianT <emilian.tapalaga@gmail.com>
2022-01-20 01:10:35 +0100

    Added some quotes and randomizer with console.log

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Proba");
+const fortuneTellerQuotes = ['Something you lost will turn up soon.',
+    'I have learned never say never.',
+    'I am not a fortune-teller.',
+    'The one you love is closer than you think.',
+    'A secret admirer will soon send you a sign of affection.',
+    'Common sense is the best fortune teller.'];
+
+const randomElement = fortuneTellerQuotes[Math.floor(Math.random() * fortuneTellerQuotes.length)];
+
+console.log(`Your fortune quote for today: ${randomElement} `);
