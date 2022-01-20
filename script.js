1b0ae5e6102f3472bdc2f13374e0f03b1b292687
EmilianT <emilian.tapalaga@gmail.com>
2022-01-20 01:29:38 +0100

    Updated the program to version 1.0.0

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-const fortuneTellerQuotes = ['Something you lost will turn up soon.',
-    'I have learned never say never.',
-    'I am not a fortune-teller.',
-    'The one you love is closer than you think.',
-    'A secret admirer will soon send you a sign of affection.',
-    'Common sense is the best fortune teller.'];
+const thingsToDo = ['Create a bet with someone that only you can win.',
+    'Go outside with a friend, get them to drin.k',
+    'Have a war with your friends in the middle of cafe.',
+    'Do a mini workout.',
+    'Make a smoothie or shake.',
+    'Actually clean up your room.'];
+const dayOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-const randomElement = fortuneTellerQuotes[Math.floor(Math.random() * fortuneTellerQuotes.length)];
+const partOfTheDay = ['Dawn', 'Morning', 'Noon', 'Afternoon', 'Dusk', 'Evening'];
 
-console.log(`Your fortune quote for today: ${randomElement} `);
+const randomElement = array => {
+    return Math.floor(Math.random() * array.length);
+}
+
+const getThingsToDo = () => {
+    let thing = thingsToDo[randomElement(thingsToDo)];
+    return thing;
+};
+
+const getDayOfTheWeek = () => {
+    let day = dayOfTheWeek[randomElement(dayOfTheWeek)];
+    return day;
+}
+
+const getPartOfTheDay = () => {
+    let part = partOfTheDay[randomElement(partOfTheDay)];
+    return part;
+}
+
+console.log(`On ${getDayOfTheWeek()} in the ${getPartOfTheDay()} you will ${getThingsToDo()}`);
